2b974225b1da2fe7b846ebfd1f29401b4c702450
Rohit <rrm913@gmail.com>
2014-04-07 18:17:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 echo 'Autobazar online car guide. ';
 echo 'Under construction';
 echo '<a href="a.php">Redirect to current diretory page</a>';
-echo '<a href="/dir1/a1.php">Redirect to dir1 diretory page</a>';
+echo '<a href="/dir1/a1.php">Move to new page new directory</a>';
 ?>
