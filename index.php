e7574893369509a69e86d3fd7d26725cf025d12b
rrm913 <rrm913@gmail.com>
2014-04-07 18:06:18 +0530

    testing

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo 'Autobazar online car guide. ';
+echo 'Under construction';
+echo '<a href="a.php">Redirect to current diretory page</a>';
+echo '<a href="dir1\a1.php">Redirect to dir1 diretory page</a>';
+?>
